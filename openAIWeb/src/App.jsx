7aabb74c52dbbc80e10c7cbb750dc914da9b29e4
Zelong Li <87303807+liz435@users.noreply.github.com>
2023-08-09 22:05:51 +0800

    fixed something

--- openAIWeb/src/App.jsx
+++ openAIWeb/src/App.jsx
@@ -1,47 +1,97 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Configuration, OpenAIApi } from 'openai';
+import key from './apikey'
 
 const configuration = new Configuration({
-  organization: 'x',
-  apiKey: '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',
+  organization: 'org-JL4yJMw7NNCmXUx2MJWyjg04',
+  apiKey: key()
 });
 
 const openai = new OpenAIApi(configuration);
 
 function App() {
-  const [count, setCount] = useState(0);
   const [message, setMessage] = useState('');
   const [chat, setChat] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
+  
   const chats = async (e, message) => {
+  
     e.preventDefault();
-    setChat([...chat, { role: 'user', content: message }]);
+    if (!message) return;
+    setIsTyping(true);
+
+
+    let msgs = chat ;
+    msgs.push({ role: 'user', content: message });
+    setChat(msgs);
     setMessage('');
 
-    try {
+    try{
+      console.log(await openai.listModels());
       const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
-        messages: [
+        max_tokens: 150,
+        message: [
           {
             role: 'system',
-            content: 'You are ChatGPT?',
+            content: 'You are ChatGPT',
+            
           },
           ...chat,
         ],
-      });
-
-      // Process the response or update your chat state with the response
-      console.log(response);
-    } catch (err) {
-      console.error(err);
+      }); 
+      console.log(response.body)
+    }catch(e){
+      console.log(e)
     }
+
+
+    
+      // const response = await openai.createChatCompletion({
+      //   model: 'gpt-3.5-turbo',
+      //   max_tokens: 150,
+      //   message: [
+      //     {
+      //       role: 'system',
+      //       content: 'You are ChatGPT',
+            
+      //     },
+      //     ...chat,
+      //   ],
+      // }).then((res) => {
+      //   console.log(res);
+      // }).catch((error) => {
+      //   console.log(error);
+      // });
   };
 
   return (
     <>
       <main>
         <h1>GPT Test</h1>
+
+        <section>
+        {chat && chat.length
+          ? chat.map((chats, index) => (
+              <p key={index} className={chats.role === "user" ? "user_msg" : ""}>
+                <span>
+                  <b>{chats.role.toUpperCase()}</b>
+                </span>
+                <span>:</span>
+                <span>{chats.content}</span>
+              </p>
+            ))
+          : ""}
+      </section>
+
+
+        <div className={isTyping ? "" : "hide"}>
+        <p>
+          <i>{isTyping ? "Typing" : ""}</i>
+        </p>
+      </div>
 
         <form onSubmit={(e) => chats(e, message)}>
           <input
@@ -51,7 +101,6 @@ function App() {
             placeholder='Type your message...'
             onChange={(e) => setMessage(e.target.value)}
           />
-          <button type='submit'>Send</button>
         </form>
 
         <div className='chat'>
